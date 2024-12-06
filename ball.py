e799aadd0829b2ccfaa45409c825b85000c86bdf
Chanunchida Dithakumarn <chanunchida.d@ku.th>
2024-12-06 09:46:02 +0700

    v1 of ball.py

--- ball.py
+++ ball.py
@@ -1,32 +1,37 @@
 import turtle
-
-def draw_ball(color, size, x, y):
-    # draw a circle of radius equals to size at x, y coordinates and paint it with color
-    turtle.penup()
-    turtle.color(color)
-    turtle.fillcolor(color)
-    turtle.goto(x,y-size)
-    turtle.pendown()
-    turtle.begin_fill()
-    turtle.circle(size)
-    turtle.end_fill()
-
-def move_ball(i, xpos, ypos, vx, vy, dt):
-    # update the x, y coordinates of ball i with velocity in the x (vx) and y (vy) components
-    xpos[i] += vx[i]*dt
-    ypos[i] += vy[i]*dt
+import math
 
 
-def update_ball_velocity(i, xpos, ypos, vx, vy, canvas_width, canvas_height, ball_radius):
-    # if the ball hits the side walls, reverse the vx velocity
-    if abs(xpos[i]) > (canvas_width - ball_radius):
-        vx[i] = -vx[i]
+class Ball:
+    def __init__(self, color, size, x, y, vx, vy):
+        self.color = color
+        self.size = size
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.canvas_width = turtle.screensize()[0]
+        self.canvas_height = turtle.screensize()[1]
 
-    # if the ball hits the ceiling or the floor, reverse the vy velocity
-    if abs(ypos[i]) > (canvas_height - ball_radius):
-        vy[i] = -vy[i]
+    def draw(self):
+        turtle.penup()
+        turtle.color(self.color)
+        turtle.fillcolor(self.color)
+        turtle.goto(self.x, self.y - self.size)
+        turtle.pendown()
+        turtle.begin_fill()
+        turtle.circle(self.size)
+        turtle.end_fill()
 
+    def move(self, dt):
+        self.x += self.vx * dt
+        self.y += self.vy * dt
 
+    def update_velocity(self):
+        if abs(self.x) > self.canvas_width:
+            self.vx = -self.vx
 
+        if abs(self.y) > self.canvas_height:
+            self.vy = -self.vy
 
 
